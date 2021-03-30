b968973740111b31cca6299439706aa611d1e233
alishkomi <aalisher.komilov@hotmail.com>
2021-03-29 20:52:09 -0500

    comments

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 import sys
 
-
+# get variables and their domains and store them in a dictionary
+# returns a dictionary with key as the variable and value as the domain
 def Variables(file):
-    variables = {}
+    variables = {} # key: variable(str), value: domain(ints)
     with open(file) as f:
         for line in f:
-            objects = line.split()
-            current_node = str(objects[0])[0]
-            values = objects[1:]
+            objects = line.split() # turn line into a list
+            current_var = str(objects[0])[0] # char letter (A, B, C, or D, ...)
+            values = objects[1:] # store domain of current variable in a list
 
-            for i in range(0, len(values)):
+            for i in range(0, len(values)): # convert values to int
                 values[i] = int(values[i])
 
-            if current_node in variables:
-                variables[current_node].append(values)
+            if current_var in variables: # check if current variable in list
+                variables[current_var].append(values) # if so, add values to key(current node)
             else:
-                variables[current_node] = values
+                variables[current_var] = values # otherwise create a key with values
     return variables
 
 
+# get conditions from a file
+# returns a dictionary with variables as keys and conditions as values stored in a list
 def Conditions(file):
-    conditions = {}
+    conditions = {} # key: variable(str), value: conditions(str)
     with open(file) as f:
-        for line in f:
-            var1 = line[0]
-            var2 = line[4]
-            if var1 not in conditions:
-                conditions[var1] = line.rstrip().rsplit("\n")
+        for line in f: # for each condition in file
+            var1 = line[0] # get first variable
+            var2 = line[4] # get second variable
+            if var1 not in conditions: # if variable not in dictionary
+                conditions[var1] = line.rstrip().rsplit("\n") # create variable and store the whole condition
             else:
-                conditions[var1].append(line.rstrip().rsplit("\n")[0])
-            if var2 not in conditions:
+                conditions[var1].append(line.rstrip().rsplit("\n")[0]) # otherwise append to list
+            if var2 not in conditions: # do same to second variable
                 conditions[var2] = line.rstrip().rsplit("\n")
             else:
                 conditions[var2].append(line.rstrip().rsplit("\n")[0])
